73f83806b646d6fcd7b159cdd8459a16a75ace2d
Elidor00 <filippo.bartolini9@gmail.com>
2020-09-23 13:38:15 +0200

    Added positional embeddings

--- positional_embeddings.py
+++ positional_embeddings.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+
 class PositionalEmbeddings(nn.Module):
     def __init__(self, emb_size, max_position, pad_index):
         super().__init__()
@@ -27,16 +28,20 @@ class PositionalEmbeddings(nn.Module):
         return embeddings
 
     def get_positions(self, batch, pad_index):
-        batch_size, sentence_max_length = batch.shape
+        batch_size, sentence_max_length = batch.shape  # 64 (batch size) x 73 (max length)
         # get positions
-        positions = torch.arange(1,sentence_max_length+1).expand(batch_size,-1).long().to(self.device)
+        positions = torch.arange(1, sentence_max_length+1).expand(batch_size, -1).long().to(self.device)
         # get mask from tensor
         mask = positions*0 + pad_index
         # fill mask
-        mask = 1-mask.ne(batch)
+        mask = ~mask.ne(batch)  # 1-mask.ne(batch)
         # mask pad words
-        positions[mask]=0
+        positions[mask] = 0
         #
         return positions
 
-#model = PositionalEmbeddings(5, 100, 0)
+# model = PositionalEmbeddings(5, 100, 0)
+
+# https://github.com/codertimo/BERT-pytorch/blob/master/bert_pytorch/model/embedding/position.py
+# https://github.com/pytorch/pytorch/issues/24826
+# https://github.com/wzlxjtu/PositionalEncoding2D/blob/master/positionalembedding2d.py
