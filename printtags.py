2a93c54471903c3a752c708535c2a8688ccfe4f3
Elidor00 <filippo.bartolini9@gmail.com>
2020-10-07 18:17:16 +0200

    Added some stats

--- printtags.py
+++ printtags.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# base use
+#   python printtags.py data/UD_2.6/it/it_isdt-ud-test.conllu
+
 # to count tag combinations:
 #   ./printtags.py ~/data/pat/train.conllu | sort | uniq -c | sort -n
 
@@ -14,14 +17,24 @@ parser.add_argument('--metadata', action='store_true', help='specify if file con
 args = parser.parse_args()
 
 rel_pos = []
+out_of_range = 0
+punct = 0
+left_threshold = -50
+right_threshold = 50
 for sentence in iter_conll(args.datafile, args.metadata, verbose=False):
     for entry in sentence:
         if entry.id > 0:
             result = []
             if args.deprel:
                 result.append(entry.deprel)
+                if entry.deprel == "punct":
+                    punct += 1
             if args.pos:
                 result.append(str(entry.pos))
                 rel_pos.append(entry.pos)
+                if entry.pos <= left_threshold or entry.pos >= right_threshold:  # empirical range [-50, 50]
+                    out_of_range += 1
             print(' '.join(result))
-print("Max relative position: ", max(rel_pos, key=abs))
+# print("Max relative position: ", max(rel_pos, key=abs))
+print("N. relative position out of empirical range: ", out_of_range)
+print("N. of punct deprel tag: ", punct)
